9b38dc5c29e76d11b0de1c5a469b6ce99176c751
sukjong2002 <sukjong2002@gmail.com>
2022-03-23 13:59:39 +0900

    Change Melon chart parsing method.

--- main.py
+++ main.py
@@ -18,33 +18,32 @@ def getSpotifyAuth():
     spotifyAuthUrl = 'https://accounts.spotify.com/api/token'
     spotifyAuthHeader = {'Authorization': 'Basic '+str2base64(spotifyCId+':'+spotifyCSec), 'Content-Type': 'application/x-www-form-urlencoded'}
     spotifyAuthData = {'grant_type': 'client_credentials'}
-    authData = requests.post(spotifyAuthUrl, headers=spotifyAuthHeader, data=spotifyAuthData).json()
+    authData1 = requests.post(spotifyAuthUrl, headers=spotifyAuthHeader, data=spotifyAuthData)
+    print(authData1.content)
+    authData = authData1.json()
     return authData['access_token']
+
+def getMelonChart():
+    #멜론 모바일 앱 일간 차트 json
+    #JSON안에 장르별 코드 들어가 있음. ex)GN000 - 장르종합
+    #daily - 일간차트, weekly - 주간차트, monthly - 월간차트
+    res = requests.get('https://m2.melon.com/m5/chart/daily/songChartList.json', params={'v': 5.0, 'gnrCode': 'GN0000', 'pageSize': 100, 'startIndex': 1}, headers=headers)
+    obj = res.json()
+    chartDict = {}
+
+    for item in obj['response']['CHARTLIST']:
+        title = item['SONGNAME']
+        artist = item['ARTISTLIST'][0]['ARTISTNAME']
+        #print(title)
+        chartDict[title] = artist
+    return chartDict
 
 spotifyUrl = 'https://api.spotify.com/v1/search'
 spotifyHeader = {'Authorization': 'Bearer '+getSpotifyAuth(), 'Content-Type': 'application/json'}
-#모바일 페이지 차트는 user-agent가 모바일로 되어 있어야 함
 headers = {'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Mobile Safari/537.36 Edg/99.0.1150.36'}
 
-#멜론 모바일 페이지 차트 get링크
-res = requests.get('https://m2.melon.com/cds/main/mobile4web/main_chartPaging.htm', headers=headers, params={'startIndex': 1, 'pageSize': 100, 'rowsCnt': 100})
-#print(res.content)
-obj = BeautifulSoup(res.content, 'html.parser')
-
-
-myDict = {}
-items = obj.findAll('li', {'class':'list_item'})
-for item in items:
-    title = item.find('p', {'class': 'title'})
-    #print(title)
-    #응답에서 쓸모없는 스페이스가 많아서 모두 지워줘야함
-    # ^: 줄 앞      \s: 스페이스       xx$: 끝          |: 양 옆의 regex를 동시에 확인
-    title = re.sub('^\s+|\s+$', '', title.text)
-    artist = item.find('span', {'class':'name'}).text
-    #print(title)
-    myDict[title] = artist
-
 i = 1
+myDict = getMelonChart()
 for item in myDict:
     print(str(i)+' '+item+' '+myDict[item])
     i = i+1
